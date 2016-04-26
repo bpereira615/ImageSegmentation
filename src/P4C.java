d33e076df0a17527e0bf099562969fe13284864a
Benjamin Hoertnagl-Pereira <bpereira615@gmail.com>
2016-04-26 19:41:29 -0400

    Pixel distance and pixel updated

---
+++ src/P4C.java
@@ -0,0 +1,83 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.util.List;
+import java.util.ArrayList;
+
+
+public class P4C {
+
+
+    /** Convert an image to a graph of Pixels with edges between
+     *  north, south, east and west neighboring pixels.
+     *  @param image the image to convert
+     *  @param pd the distance object for pixels
+     *  @return the graph that was created
+     */
+    static WGraph<Pixel> imageToGraph(BufferedImage image, PixelDistance pd) {
+        //TODO: Distance<Pixel> confusion as second parameter
+        return null;
+    }
+
+
+    /** Return a list of edges in a minimum spanning forest by
+     *  implementing Kruskal's algorithm using fast union/finds.
+     *  @param g the graph to segment
+     *  @param kvalue the value to use for k in the merge test
+     *  @return a list of the edges in the minimum spanning forest
+     */
+
+    static List<WEdge<Pixel>> segmenter(WGraph<Pixel> g, double kvalue) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+        final int gray = 0x202020;
+
+        try {
+          // the line that reads the image file
+
+            BufferedImage image = ImageIO.read(new File(args[0]));
+            WGraph<Pixel> g = imageToGraph(image, new PixelDistance());
+            List<WEdge<Pixel>> res = segmenter(g, Double.parseDouble(args[1]));
+
+            System.out.print("result =  " + res.size() + "\n");
+            System.out.print("NSegments =  "
+                             + (g.numVerts() - res.size()) + "\n");
+
+            // make a background image to put a segment into
+            for (int i = 0; i < image.getHeight(); i++) {
+                for (int j = 0; j < image.getWidth(); j++) {
+                    image.setRGB(j, i, gray);
+                }
+            }
+
+            // After you have a spanning tree connected component x, 
+            // you can generate an output image like this:
+            List<GVertex<Pixel>> x = null;
+            for (GVertex<Pixel> i: x)  {
+                Pixel d = i.data();
+                image.setRGB(d.col(), d.row(), d.value());
+            }
+
+            File f = new File("output.png");
+            ImageIO.write(image, "png", f);
+
+            // You'll need to do that for each connected component,
+            // writing each one to a different file, clearing the
+            // image buffer first
+
+        } catch (IOException e) {
+            System.out.print("Missing File!\n");
+
+            // log the exception
+            // re-throw if desired
+        }
+    }
+
+}
