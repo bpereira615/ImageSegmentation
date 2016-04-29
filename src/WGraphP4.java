80e63136231399af38ccef8da1f3a13b8d5293b6
carrolm <lydiacarroll@lydias-mbp.mtw.jhu.edu>
2016-04-29 19:22:49 -0400

    Checkstyle changes for P4C.

--- src/WGraphP4.java
+++ src/WGraphP4.java
@@ -2,7 +2,6 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Iterator;
-// import java.util.PriorityQueue;
 import java.util.ListIterator;
 import java.util.HashSet;
 import java.util.Stack;
