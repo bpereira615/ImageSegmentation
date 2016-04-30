56989b9adb5d95dce6f7055692cd520290c37c20
carrolm <lydiacarroll@lydias-mbp.mtw.jhu.edu>
2016-04-29 20:28:21 -0400

    Added comments for checkstyle.

--- src/PixelDistance.java
+++ src/PixelDistance.java
@@ -1,15 +1,33 @@
-/** Implementation of the Distance interface.
+/********************************************************************
+ * Lydia Carroll, Benjamin Hoertnagl-Pereira, Ryan Walter
+ * JHED: lcarro12, bhoertn1, rwalte25
+ * lcarro12 @jhu.edu, bhoertn1@jhu.edu, rwalte25@jhu.edu
  *
- */
-public class PixelDistance implements Distance<Pixel>{
-	
-	/** Empty constructor for PixelDistance */
-	public PixelDistance() {
+ * 600.226.01 | CS226 Data Structures
+ * Project 4 - Image Segmentation
+ *******************************************************************/
 
-	}
-	public double distance (Pixel one, Pixel two) {
-		double square = Math.pow(one.r() - two.r(), 2) + Math.pow(one.g() - two.g(), 2) 
-			+ Math.pow(one.b() - two.b(), 2);
-		return Math.pow(square, 0.5);
-	}
+/**
+ * Implementation of the Distance interface.
+ * @param one first pixel
+ * @param two second pixel
+ * @return color difference between pixels
+ */
+public class PixelDistance implements Distance<Pixel> {
+
+    /** Take the square root of the squares. */
+    static final double PT5 = 0.5;
+
+    /** Empty constructor for PixelDistance. */
+    public PixelDistance() {
+
+    }
+
+    /** Finds the color distance between two pixels. */
+    public double distance (Pixel one, Pixel two) {
+        double square = Math.pow(one.r() - two.r(), 2) 
+            + Math.pow(one.g() - two.g(), 2) 
+            + Math.pow(one.b() - two.b(), 2);
+        return Math.pow(square, PT5);
+    }
 }
--- src/WEdge.java
+++ src/WEdge.java
@@ -7,9 +7,10 @@
  * Project 4 - Image Segmentation
  *******************************************************************/
 
-/** Implementation of an edge class (for graphs), could be directed or not.
+/**
+ * Implements a weighted edge class for graph.
+ * @param VT data type for vertices.
  */
-
 public class WEdge<VT> implements Comparable<WEdge<VT>> {
 
     /** Starting vertex of an edge. */
@@ -19,9 +20,10 @@ public class WEdge<VT> implements Comparable<WEdge<VT>> {
     /** Weight of edge */
     private double weight;
 
-    /** Create an undirected edge.
-     *  @param u the start
-     *  @param v the end
+    /** 
+     * Create an undirected edge.
+     * @param u the start
+     * @param v the end
      */
     public WEdge(GVertex<VT> u, GVertex<VT> v, double w) {
         this.source = u;
@@ -29,46 +31,51 @@ public class WEdge<VT> implements Comparable<WEdge<VT>> {
         this.weight = w;
     }
 
-    /** Is a vertex incident to this edge.
-     *  @param v the vertex
-     *  @return true if source or end, false otherwise
+    /** 
+     * Is a vertex incident to this edge.
+     * @param v the vertex
+     * @return true if source or end, false otherwise
      */
     public boolean isIncident(GVertex<VT> v) {
         return this.source.equals(v) || this.end.equals(v);
     }
 
-    /** Get the starting endpoint vertex.
-     *  @return the vertex
+    /** 
+     * Get the starting endpoint vertex.
+     * @return the vertex
      */
     public GVertex<VT> source() {
         return this.source;
     }
 
-    /** Get the ending endpoint vertex.
-     *  @return the vertex
+    /** 
+     * Get the ending endpoint vertex.
+     * @return the vertex
      */
     public GVertex<VT> end() {
         return this.end;
     }
 
-    /** Get the weight of the edge.
-     *  @return the weight
+    /** 
+     * Get the weight of the edge.
+     * @return the weight
      */
     public double weight() {
         return this.weight;
     }
 
-    /** Create a string representation of the edge.
-     *  @return the string as (source,end)
+    /** 
+     * Create a string representation of the edge.
+     * @return the string as (source,end)
      */
     public String toString() {
-        //TODO: need decimal formatting?
         return "(" + this.source + "," + this.end + "," + this.weight + ")";
     }
 
-    /** Check if two edges are the same.
-     *  @param other the edge to compare to this
-     *  @return true if directedness and endpoints match, false otherwise
+    /** 
+     * Check if two edges are the same.
+     * @param other the edge to compare to this
+     * @return true if directedness and endpoints match, false otherwise
      */
     @Override   
     public boolean equals(Object other) {
@@ -94,26 +101,27 @@ public class WEdge<VT> implements Comparable<WEdge<VT>> {
     }
 
 
-    /** Compares two edges based on weights.
-     *  @return the hashCode
+    /** 
+     * Compares two edges based on weights.
+     * @return the hashCode
      */
     @Override
     public int compareTo(WEdge<VT> other) {
-        //TODO: what to return when not WEdge?
         // if (other instanceof WEdge) {
-            WEdge<VT> e = other; //(WEdge)
-            if (this.weight < e.weight) {
-                return -1;
-            } else if (this.weight > e.weight) {
-                return 1;
-            } else {
-                return 0;
+        WEdge<VT> e = other; //(WEdge)
+        if (this.weight < e.weight) {
+            return -1;
+        } else if (this.weight > e.weight) {
+            return 1;
+        } else {
+            return 0;
             }
     }
 
 
 
-    /** Make a hashCode based on the toString.
+    /** 
+     * Make a hashCode based on the toString.
      *  @return the hashCode
      */
     public int hashCode() {
